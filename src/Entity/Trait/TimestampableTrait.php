907448aa565597170af2c086827a5342535b2430
CedricB40 <bazinc40@gmail.com>
2026-07-25 09:32:49 +0200

    feat (video) : ajout du trait TimestampableTrait pour createdAt/updatedAt automatiques

--- src/Entity/Trait/TimestampableTrait.php
+++ src/Entity/Trait/TimestampableTrait.php
@@ -6,10 +6,10 @@ use Doctrine\ORM\Mapping as ORM;
 
 trait TimestampableTrait
 {
-    #[ORM\Column]
+    #[ORM\Column] //date de création
     private ?\DateTimeImmutable $createdAt = null;
 
-    #[ORM\Column]
+    #[ORM\Column] //date de modification
     private ?\DateTimeImmutable $updatedAt = null;
 
     public function getCreatedAt(): ?\DateTimeImmutable
@@ -22,14 +22,14 @@ trait TimestampableTrait
         return $this->updatedAt;
     }
 
-    #[ORM\PrePersist]
+    #[ORM\PrePersist] //appelé par Doctrine juste avant le premier persist (création)
     public function setCreatedAtValue(): void
     {
         $this->createdAt = new \DateTimeImmutable();
         $this->updatedAt = new \DateTimeImmutable();
     }
 
-    #[ORM\PreUpdate]
+    #[ORM\PreUpdate] //appelé par Doctrine juste avant chaque flush qui modifie l'entité
     public function setUpdatedAtValue(): void
     {
         $this->updatedAt = new \DateTimeImmutable();
